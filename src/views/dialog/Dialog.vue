d059a91a4748242a62b6a27dfefe0e9fcc0aba34
Onur Inan (A) <o00834769@china.huawei.com>
2023-06-01 14:52:34 +0300

    dialog component initial commit and and router arrangement

---
+++ src/views/dialog/Dialog.vue
@@ -0,0 +1,26 @@
+<template>
+    <HwDialog> 
+        <template v-slot:header>Use Google's location service?</template>
+        <template v-slot:content>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.  </template>
+        <!-- <template v-slot:footer>deneme </template> -->
+    </HwDialog>
+</template>
+
+
+<script>
+import HwDialog from '../../components/library/select/HwDialog.vue';
+
+export default {
+    name: "Dialog",
+    data() {
+        return {
+        };
+    },
+    components: { HwDialog } 
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
