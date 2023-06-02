ef36f7670f4d696f5ab2c4b247fb494313e78b9f
Onur Inan (A) <o00834769@china.huawei.com>
2023-06-02 15:53:27 +0300

    Dialog component rendered page

--- src/views/dialog/Dialog.vue
+++ src/views/dialog/Dialog.vue
@@ -1,8 +1,10 @@
 <template>
     <HwDialog> 
-        <template v-slot:header>Use Google's location service?</template>
-        <template v-slot:content>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.  </template>
-        <!-- <template v-slot:footer>deneme </template> -->
+        <template v-slot:header>Use Huawei's location service?</template>
+        <template v-slot:content>Let Huawei help apps determine location. This means sending anonymous location data to Huawei, even when no apps are running. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos et suscipit totam quis, provident delectus, eos laborum veniam accusantium corrupti animi doloremque sit quaerat sapiente iusto facere, aperiam dolorum.
+        Atque, maxime deserunt error corporis, sequi fugiat dignissimos delectus, voluptatum deleniti aliquam praesentium ullam. Voluptate rem, deserunt nobis voluptatem ad dignissimos aspernatur eaque delectus sed eligendi, itaque voluptatibus doloremque neque!
+        Itaque odio autem odit magni illum? Ipsum sapiente, aliquam id voluptatibus cumque odit molestiae sit corrupti quos, placeat quas tempore magni quibusdam quidem, dignissimos nulla ab asperiores illum reprehenderit minima.  </template>
+        
     </HwDialog>
 </template>
 
