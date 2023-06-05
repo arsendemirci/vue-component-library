a3c0568850b049420baea465fc7fd6221be26b14
Engin Karatas <engin.karatas@huawei.com>
2023-06-05 16:27:38 +0300

    Create card, advance card with binding
    Created simple card and advanced card appearance  in template and simply bind data for future developments.

---
+++ src/components/library/card/HwCard.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="card-wrapper">
+    <div class="card-container">
+      <div class="card-default">
+        <h3>{{ title }}</h3>
+        <div class="card-default-content">
+          <p>
+            {{ contentText }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card-wrapper">
+    <div class="card-container">
+      <div class="card-advanced">
+        <h1 class="header">{{ header }}</h1>
+        <h1 class="title">{{ title }}</h1>
+        <h1 class="subtitle">{{ subtitle }}</h1>
+        <div class="card-image-wrapper">
+          <img src="../../../assets/1000x300.png" alt="" />
+        </div>
+        <button class="hw-button orange">Save</button>
+        <button class="hw-button gray">Cancel</button>
+
+        <div class="card-footer">
+          <p>{{ footerText }}</p>
+        </div>
+        <!-- <div class="card-flex">
+          <button class="button-mini flex-right"><i>🔽</i></button>
+        </div> -->
+        <div class="card-accordion-wrapper" style="visibility: none">
+          <hr class="hw-divider" />
+          <p class="accordion-text">
+            {{ accordionText }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  reactive,
+  computed,
+  defineProps,
+  defineEmits,
+  watch,
+  onMounted,
+} from "vue";
+const props = defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  contentText: {
+    type: String,
+    required: true,
+  },
+  footerText: {
+    type: String,
+    required: true,
+  },
+  accordionText: {
+    type: String,
+    required: true,
+  },
+});
+// const state = reactive({
+//   title: "sd",
+//   accordionToggle: false,
+// });
+</script>
+
+<style scoped lang="scss">
+.card-wrapper {
+  width: 350px;
+  position: relative;
+  -webkit-tap-highlight-color: transparent;
+  background-color: palette-color-level(white, 100);
+  border-radius: palette-radius-level(5);
+  border: solid 1px palette-color-level(grey, 15);
+  box-sizing: border-box;
+  clear: both;
+  //FIXME: display block not fit for card elements.(in one line may one ore more cards)
+  //display: block
+  font-family: inherit;
+  font-size: palette-font-size-level(5);
+  font-weight: palette-font-weight-level(5);
+  line-height: 40px;
+  padding: palette-space-level(15), palette-space-level(15);
+  position: static;
+  text-align: left !important;
+  user-select: none;
+  white-space: normal;
+
+  .card-container {
+    padding: palette-padding-level(10, 15);
+    p {
+      line-height: palette-space-level(20);
+      font-weight: palette-font-weight-level(3);
+    }
+    .card-advanced {
+      .header {
+        font-size: palette-font-size-level(10);
+        font-weight: palette-font-weight-level(5);
+        line-height: palette-space-level(40);
+      }
+      .title {
+        font-size: palette-font-size-level(5);
+        font-weight: palette-font-weight-level(5);
+        line-height: palette-space-level(20);
+      }
+      .subtitle {
+        color: gray;
+        font-size: palette-font-size-level(2);
+        font-weight: palette-font-weight-level(3);
+        line-height: palette-space-level(20);
+      }
+      .card-image-wrapper {
+        padding-bottom: palette-space-level(10);
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+  .hw-button {
+    color: palette-color-level(white, 100);
+    border: none;
+    border-radius: palette-radius-level(5);
+    width: 100%;
+    height: 30px;
+    cursor: pointer;
+    //huawei color button
+  }
+
+  .hw-button.orange {
+    background-color: palette-color-level(orange, 10);
+  }
+  .hw-button.gray {
+    background-color: palette-color-level(grey, 20);
+  }
+
+  .card-footer {
+    padding-top: palette-space-level(20);
+    padding-bottom: palette-space-level(10);
+    p {
+      font-weight: palette-font-weight-level(5);
+    }
+  }
+  .card-flex {
+    //one item one line flex
+    display: flex;
+    justify-content: flex-end;
+    padding-bottom: palette-space-level(20);
+  }
+
+  .button-mini {
+    width: 30px;
+    height: 30px;
+    border: solid palette-color-level(grey, 15);
+    border-radius: palette-radius-level(5);
+  }
+
+  .hw-divider {
+    margin: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #000;
+  }
+
+  .card-accordion-wrapper {
+    .accordion-text {
+      font-size: palette-font-size-level(4);
+      font-weight: palette-font-weight-level(3);
+      line-height: palette-space-level(25);
+    }
+  }
+}
+</style>
---
+++ src/views/card/Card.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="wrapper" @click="checkboxIsHeadern">
+    <h1 slot="header">Card Components</h1>
+    <hr />
+    <div>
+      <hw-card
+        type="simple"
+        :header="header"
+        :title="title"
+        :subtitle="subtitle"
+        :content-text="contentText"
+        :footer-text="footerText"
+        :accordion-text="accordionText"
+      ></hw-card>
+    </div>
+    <p>{{ checkboxIsHeader }}</p>
+
+    <span>
+      <input
+        type="checkbox"
+        id="isHeader"
+        name="isHeader"
+        v-model="checkboxIsHeader"
+      />
+      <label for="isHeader">Header</label>
+    </span>
+  </div>
+</template>
+
+<script>
+import HwCard from "@lib/card/HwCard.vue";
+export default (await import("vue")).defineComponent({
+  data() {
+    return {
+      header: "Header",
+      title: "Title",
+      subtitle: "subtitle",
+      contentText:
+        "Content lorem ipsum dolor sit amet consectetur adipisicing elit.            Nemo laudantium magnam, aut illum libero, harum enim quas magni vel            doloremque dolorus",
+      footerText: "Here is footer text lorem ipsum dolor sit amet",
+      accordionText:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quibusdam voluptatum, voluptatibus.",
+      checkboxIsHeader: true,
+    };
+  },
+  components: {
+    HwCard,
+  },
+  methods: {
+    checkboxIsHeadern() {
+      console.log("asf");
+      this.checkboxIsHeader = true;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: palette-space-level(35);
+  font-size: large;
+  header {
+    h1,
+    h3 {
+      text-shadow: 1px 1px 3px rgb(255, 255, 255);
+    }
+    h1 {
+      margin: palette-space-level(50);
+    }
+    h3 {
+      margin: 0;
+    }
+  }
+  > div {
+    padding: palette-space-level(15) palette-space-level(20);
+    flex-direction: column;
+    background: palette-color-level(white, 100);
+    border-radius: palette-radius-level(5);
+    box-shadow: 0 0 6px rgba(190, 190, 190, 0.44);
+    display: flex;
+    gap: palette-space-level(20);
+    background: floralwhite;
+    width: 100%;
+    max-width: 1000px;
+    .v-model {
+      .value {
+        font-weight: palette-font-weight-level(5);
+        font-size: large;
+      }
+    }
+
+    .option {
+      display: flex;
+
+      &:hover {
+        .option-value {
+          border-color: palette-color-level(white, 100);
+          border-width: palette-space-level(4);
+        }
+      }
+      &-value {
+        padding-right: palette-space-level(10);
+        border-right: 1px solid palette-color-level(primary-alt, 100);
+        transition: all 0.2s cubic-bezier(0.5, 0, 0, 1.25);
+      }
+      &-text {
+        padding-left: palette-space-level(10);
+      }
+    }
+  }
+}
+</style>
