c848e1d3e2c02ff2d1a5cb530b18c94e31ebeadd
Onur Inan (A) <o00834769@china.huawei.com>
2023-06-19 10:59:17 +0300

    demo page developed for modal component

--- src/views/dialog/Dialog.vue
+++ src/views/dialog/Dialog.vue
@@ -1,32 +1,169 @@
 <template>
-    <HwDialog> 
+  <div class="wrapper">
+    <h1>Dialog Component</h1>
+
+    <DialogDemo />
+
+    <div>
+      <h2>Default Dialog Component</h2>
+      <p>
+        Dialog components can provide some content and action input from user when needed. This is default dialog and
+        centered on the page whit animation.
+      </p>
+
+      <div class="select-wrapper">
+        <select class="select" name="positions" id="positions" @input="setSelected($event.target.value)">
+          <option value="center">center</option>
+          <option value="top">top</option>
+          <option value="right">right</option>
+          <option value="bottom">bottom</option>
+          <option value="left">left</option>
+        </select>
+
+        <select class="select" name="sizes" id="sizes" @input="setSize($event.target.value)">
+          <option value="xs">xs</option>
+          <option value="sm">sm</option>
+          <option value="md" selected>md</option>
+          <option value="lg">lg</option>
+          <option value="xl">xl</option>
+        </select>
+
+        <div class="checkbox-wrapper">
+          <input
+            class="checkbox-input"
+            v-model="selectedBackdrop"
+            type="checkbox"
+            id="backdrop"
+            name="selectedBackdrop"
+            :value="selectedBackdrop"
+            />
+          <label for="backdrop">Backdrop</label>
+        </div>
+
+        <div class="checkbox-wrapper">
+          <input
+            class="checkbox-input"
+            checked
+            v-model="selectedAnimation"
+            type="checkbox"
+            id="animation"
+            name="selectedAnimation"
+            :value="selectedAnimation"
+            />
+          <label for="animation">Animation</label>
+        </div>
+      </div>
+
+      <HwDialog :position="selectedPosition" :size="selectedSize" :animation="selectedAnimation" :backdrop="selectedBackdrop">
         <template v-slot:header>Use Huawei's location service?</template>
-        <template v-slot:content>Let Huawei help apps determine location. This means sending anonymous location data to Huawei, even when no apps are running. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos et suscipit totam quis, provident delectus, eos laborum veniam accusantium corrupti animi doloremque sit quaerat sapiente iusto facere, aperiam dolorum.
-        Atque, maxime deserunt error corporis, sequi fugiat dignissimos delectus, voluptatum deleniti aliquam praesentium ullam. Voluptate rem, deserunt nobis voluptatem ad dignissimos aspernatur eaque delectus sed eligendi, itaque voluptatibus doloremque neque!
-        Itaque odio autem odit magni illum? Ipsum sapiente, aliquam id voluptatibus cumque odit molestiae sit corrupti quos, placeat quas tempore magni quibusdam quidem, dignissimos nulla ab asperiores illum reprehenderit minima.  
+        <template v-slot:content>Let Huawei help apps determine location. This means sending anonymous location data to
+          Huawei, even when no apps are running. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos
+          et suscipit totam quis, provident delectus, eos laborum veniam accusantium corrupti animi doloremque sit quaerat
+          sapiente iusto facere, aperiam dolorum.
+          Atque, maxime deserunt error corporis, sequi fugiat dignissimos delectus, voluptatum deleniti aliquam
+          praesentium ullam. Voluptate rem, deserunt nobis voluptatem ad dignissimos aspernatur eaque delectus sed
+          eligendi, itaque voluptatibus doloremque neque!
+          Itaque odio autem odit magni illum? Ipsum sapiente, aliquam id voluptatibus cumque odit molestiae sit corrupti
+          quos, placeat quas tempore magni quibusdam quidem, dignissimos nulla ab asperiores illum reprehenderit minima.
         </template>
-        
+
         <template v-slot:footer-agree>Agree</template>
         <template v-slot:footer-cancel>Cancel</template>
-    
-    </HwDialog>
+
+      </HwDialog>
+    </div>
+  </div>
 </template>
 
 
 <script>
 import HwDialog from '../../components/library/select/HwDialog.vue';
+import DialogDemo from './DialogDemo.vue';
 
 export default {
-    name: "Dialog",
-    data() {
-        return {
-        };
+  name: "Dialog",
+  data() {
+    return {
+      selectedPosition: "center",
+      selectedSize: "md",
+      selectedBackdrop: false,
+      selectedAnimation: true
+    };
+  },
+  methods: {
+    setSelected(selectedValue) {
+      this.selectedPosition = selectedValue
     },
-    components: { HwDialog } 
+
+    setSize(selectedSize) {
+      this.selectedSize = selectedSize
+    },
+
+    setBackdrop(event) {
+      this.selectedBackdrop = event.target.value
+      console.log(this.selectedBackdrop);
+      // return this.selectedBackdrop
+    },
+
+    setAnimation(selectedAnimation) {
+      this.selectedAnimation = selectedAnimation
+      console.log(this.selectedAnimation);
+    }
+  },
+  components: { HwDialog, DialogDemo }
 }
 
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: palette-space-level(35);
+  font-size: large;
 
-</style>
+  header {
+    h1 {
+      margin: palette-space-level(50);
+    }
+  }
+
+  >div {
+    padding: palette-space-level(15) palette-space-level(20);
+    flex-direction: column;
+    background: palette-color-level(white, 100);
+    border-radius: palette-radius-level(5);
+    box-shadow: 0 0 6px rgba(190, 190, 190, 0.44);
+    display: flex;
+    gap: palette-space-level(20);
+    background: floralwhite;
+    width: 100%;
+    max-width: palette-space-level(1000);
+
+    .select-wrapper {
+      display: flex;
+      justify-content: space-around;
+
+      .select {
+      width: 20%;
+      padding: palette-space-level(5);
+      border-radius: palette-radius-level(2);
+    }
+     
+    .checkbox-wrapper {
+      display: flex;
+      align-items: center;
+
+      .checkbox-input {
+        margin-right: palette-space-level(5);
+      }
+    }
+
+    }
+  }
+
+  &:last-child {
+    margin-bottom: palette-space-level(400);
+  }
+}</style>
---
+++ src/views/dialog/DialogDemo.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title"><span>👀</span>Overview</h2>
+          <p class="card-text">This project is actually a part of a vue3 component library project that is under development right now. ag-dialog is a custom component that takes advantage of many features of Vue3.</p>
+        </div>
+      </div>
+      <div class="card">
+       <div class="card-body">
+        <h2 class="card-title"><span>🔫</span>Getting Started</h2>
+        <p class="card-text">For ag-dialog there are some props and their default values are avaliable. You can see the other values on props table. See the code example below:</p>
+       </div>
+      </div>
+      <div class="card-pre">
+        <pre>
+        <code class="language-html">
+          &lt;HwDialog position="center" size="md" :backdrop="false" :animation="true" &gt;
+          &lt;/HwDialog&gt; 
+        </code>
+     </pre>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title"><span>💣</span>Features</h2>
+          <ul class="card-list">
+            <li>Header, body and footer contents supported customization by slots</li>
+            <li>Dialog on hover displays a scroller when content exceeds viewport</li>
+            <li>Open and close animations</li>
+            <li>Optional animation display</li>
+            <li>Backdrop opacity and elevated design</li>
+            <li>Positioning options</li>
+            <li>Size options</li>
+            <li>Open and close methods and events</li>
+            <li>No close on backdrop click option</li>
+          </ul>
+  
+        </div>
+      </div>
+      <h2 class="card-title"><span>💊</span>Props</h2>
+      <table class="table">
+        <thead>
+        <tr>
+          <th>Props</th>
+          <th>Type</th>
+          <th>Default</th>
+          <th>Options</th>
+          <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>position</td>
+          <td>String</td>
+          <td>center</td>
+          <td>center, top, right, bottom, left</td>
+          <td>Position options for modal component</td>
+        </tr>
+        <tr>
+          <td>size</td>
+          <td>String</td>
+          <td>md</td>
+          <td>xs, sm, md, lg, xl</td>
+          <td>Size options for modal component</td>
+
+        </tr>
+        <tr>
+          <td>backdrop</td>
+          <td>Boolean</td>
+          <td>false</td>
+          <td>true, false</td>
+          <td>Backdrop flag for closing modal when outside click</td>
+        </tr>
+        <tr>
+          <td>animation</td>
+          <td>Boolean</td>
+          <td>true</td>
+          <td>true, false</td>
+          <td>Animation flag for modal openning and closing</td>
+        </tr>
+  
+        </tbody>
+      </table>
+    </div>
+  </template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: palette-space-level(1000);
+  margin: 0 auto;
+  padding: palette-space-level(20);
+}
+
+h1 {
+  font-size: palette-font-size-level(14);
+  margin-bottom: palette-space-level(20);
+}
+
+.card {
+  border: 1px solid palette-color-level(grey, 10);
+  border-radius:  palette-radius-level(3);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: palette-space-level(20);
+  background-color: palette-color-level(white, 100);;
+  .card-text{
+    padding: 8px;
+  }
+}
+
+.card-body {
+  padding: 20px;
+}
+.card-list{
+  list-style-type: square;
+  margin: 10px;
+  padding: 1px 0 2px 25px;
+}
+.card-pre{
+   margin-bottom: 20px;
+  pre {
+    line-height: 1.2;
+    background-color: palette-color-level(primary-dark, 100);
+    border-radius:  palette-radius-level(3);
+    white-space: pre-wrap;
+    overflow: auto;
+    color:palette-color-level(primary-light, 100);
+  }
+  code {
+    display: inline;
+    padding: 0;
+    margin: 0;
+    overflow: visible;
+    word-wrap: normal;
+    background-color: transparent;
+    border: 0;
+  }
+}
+h2 {
+  font-size: palette-font-size-level(9);
+  margin: 0;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 20px;
+  background-color: palette-color-level(white, 100);
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid palette-color-level(grey, 60);
+}
+
+th {
+  background-color: palette-color-level(grey, 80);
+  font-weight: palette-font-weight-level(5);
+}
+
+</style>
