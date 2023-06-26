91ca293097ea8db577cb92ae2875ff9e58b77133
k84255567 <kemal.oncel@huawei.com>
2023-06-26 17:39:41 +0300

    added box shadow

--- src/components/library/tab/HwTab.vue
+++ src/components/library/tab/HwTab.vue
@@ -96,6 +96,9 @@ onMounted(()=>{
 
 <style scoped lang="scss">
 .tabs{
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: 0.3s box-shadow ease;
+  &:hover { box-shadow: 0 2px 3px rgba(0, 0, 0, 0.23), 0 1px 1px rgba(0, 0, 0, 0.19); }
   &.verticalTabParent{
     display: flex;
     .tabs-content{
